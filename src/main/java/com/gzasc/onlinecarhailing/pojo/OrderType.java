7c7b5755f96c1bdf60b69cc8b72329fa04a84cc7
rzgg <qieierqpuu@gmail.com>
2024-03-22 21:35:06 +0800

    在思考订单的创建和撤销和接取应该怎么设计表和类

---
+++ src/main/java/com/gzasc/onlinecarhailing/pojo/OrderType.java
@@ -0,0 +1,11 @@
+package com.gzasc.onlinecarhailing.pojo;
+
+//一个用来记录订单类型的类，用常量来区别订单类型。
+public class OrderType {
+
+    public static final Integer TYPE_PASSENGER = 0;
+    public static final Integer TYPE_DRIVER = 1;
+    public static final Integer TYPE_OFFICIAL = 2;
+
+
+}
