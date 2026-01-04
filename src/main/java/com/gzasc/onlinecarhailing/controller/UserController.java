ce3d5baa976829691b5661427b3fb07de2a63b18
gg <qieierqpuu@googlemail.com>
2026-01-04 12:08:31 +0800

    删除一些不必要的注释。

--- src/main/java/com/gzasc/onlinecarhailing/controller/UserController.java
+++ src/main/java/com/gzasc/onlinecarhailing/controller/UserController.java
@@ -260,29 +260,6 @@ public class UserController {
 
     }
 
-//
-//    //    插入或重新进行一个聊天室
-//    @RequestMapping("/chating")
-//    public Result buildChatRoom(Chaters chaters) {
-//
-//        Passenger passenger = chaters.getPassenger();
-//        Driver driver = chaters.getDriver();
-//
-////      得到聊天室，如果聊天室的id为null，就说明没有。此时才真正的插入。就返回原来的聊天室。
-////        会在service层判断。
-//
-//        ChatRoom chatRoom1 = chatRoomService.createChatRoom(passenger, driver);
-//
-//        if (chatRoom1.getId() != null) {
-//
-//            chatRoom1.setName(passenger.getName());
-//
-//            return Result.success("进行聊天", chatRoom1);
-//
-//        } else return Result.error("失败");
-//
-//
-//    }
 
     //    计算发出的接单的预计价格
     @RequestMapping("/countPriceOfAddress")
